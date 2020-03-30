448ce3d9d1fc24b4e8d3facd34a460933d2d831e
Ummimath Farisha <farishaummimath@gmail.com>
2020-03-31 00:18:45 +0530

    Changes to index

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors())
 
 // Serve static files from the React app
 
-app.use(express.static(path.join(__dirname, 'client/build')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('/api/users', function(req,res){
@@ -23,6 +22,7 @@ app.get('/api/users', function(req,res){
         }
     })
 })
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
